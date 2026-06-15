722564624f67947686ee649d4a1eddb5874b8cb5
joansalazar550-ship-it <joansalazar550@gmail.com>
2026-06-15 18:47:33 -0500

    Add App component with ETFCard instances

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,14 @@
+import ETFCard from './components/ETFCard';
+
+function App() {
+  return (
+    <div>
+      <h1>Mis ETFs</h1>
+      <ETFCard />
+      <ETFCard />
+      <ETFCard />
+    </div>
+  );
+}
+
+export default App;
